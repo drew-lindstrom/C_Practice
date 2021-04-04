19efbed540595daf0e4b476b29a129df85399d04
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-04-03 22:03:26 -0500

    Implemented graph.

---
+++ graph.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct AdjListNode
+{
+    int dest;
+    struct AdjListNode *next;
+};
+
+struct AdjList
+{
+    struct AdjListNode *head;
+};
+
+struct Graph
+{
+    int V;
+    struct AdjList *array;
+};
+
+struct AdjListNode *newAdjListNode(int dest)
+{
+    struct AdjListNode *newNode =
+        (struct AdjListNode *)malloc(sizeof(struct AdjListNode));
+    newNode->dest = dest;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Graph *createGraph(int V)
+{
+    struct Graph *graph =
+        (struct Graph *)malloc(sizeof(struct Graph));
+    graph->V = V;
+
+    graph->array =
+        (struct AdjList *)malloc(V * sizeof(struct AdjList));
+
+    int i;
+    for (i = 0; i < V; ++i)
+        graph->array[i].head = NULL;
+
+    return graph;
+}
